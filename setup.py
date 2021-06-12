5899b8a2761054bcca22ee10860bf65b65adb19b
Hassan Tahir <info.hassantahir@gmail.com>
2021-06-12 12:05:43 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
+#importing libraries 
 import os, math, sys
 
-#OS_bit = (round(math.log(sys.maxint,2)+1))  # get the bit
 
-os.system("sudo apt-get install python-pip && sudo apt-get install tor")   # installing dependencies
+# installing dependencies for firefox
+
+os.system("sudo apt-get install python-pip && sudo apt-get install tor")   
 os.system("pip install -U selenium")
 os.system("pip install Pysocks")
 os.system("pip install pyvirtualdisplay && apt-get install xvfb")
 
-#print("\n \n {} \n \n".format(OS_bit))
 
+#printing the core values of the OS
 
-os.system('firefox -v > tmp')                  # store result of firefox -v in tmp
-result   =  open('tmp', 'r').read()            # result var reads the output
-marker   = result.find('Firefox') + 8          # marker marks the 8th letter from the word "Firefox"
+os.system('firefox -v > tmp')                  # storing the result of firefox in -v along tmp
+result   =  open('tmp', 'r').read()            # results in variables reads the output of the OS
+marker   = result.find('Firefox') + 8          # 8th letter marks the marks from the word "Firefox"
 version  = result[marker:].splitlines()[0]     # spliting the output, the version is something like aa.bb.cc
 a,b,c = version.split(".")                     # a is the var with the aa
-os.remove('tmp')                               # removing the temporary file
+os.remove('tmp')                               # deleting temp files for data optimization
 
 FirefoxVersion = int(a)
 second = 0
 
+# adding conditional formating
 if FirefoxVersion  < 53:
 
     first = 16
@@ -34,6 +37,7 @@ elif FirefoxVersion > 54:
 
     first = 19
 
+# fetching drivers for the new prev release of geckodrivers
 os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.{}.{}/geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,first,second,OS_bit))
 os.system("tar -xvzf geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,OS_bit))
 os.system("rm geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,OS_bit))
